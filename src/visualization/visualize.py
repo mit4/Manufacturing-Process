de571d3381613e389640800f783ee83b9bb4af6f
mit4 <88436281+mit4@users.noreply.github.com>
2023-09-06 17:59:33 +0530

    added plots and link in readme

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import os
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -10,7 +11,7 @@ data.info()
 # grouped_columns = {first_word: [col for col in data.columns if col.startswith(first_word + '.')] for first_word in set(col.split('.')[0] for col in data.columns)}
 
 
-def plot_columns_by_prefix(data, prefix, title):
+def plot_columns_by_prefix(data, prefix):
     """
     Create line plots for columns with a specified prefix in their names.
 
@@ -22,78 +23,84 @@ def plot_columns_by_prefix(data, prefix, title):
     Returns:
         None
     """
+    output_dir = "../../reports/figures/plots"
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+
     columns = [col for col in data.columns if col.startswith(prefix)]
 
     plt.figure(figsize=(12, 6))  # Adjust figure size if needed
-    plt.title(f"Line Plot for {title}")
+    plt.title(f"Line Plot for {prefix}")
     for column in columns:
-        plt.plot(data.index, data[column], label=column)
+        plt.scatter(
+            data.index, data[column], s=2, label=column
+        )  # s specifies marker size
     plt.xlabel("Datetime")
     plt.ylabel("Value")
     plt.legend(loc="best")
     plt.grid(True)
+
+    # Save the plot to a file
+    output_file = os.path.join(output_dir, f"scatter_plot_{prefix}.png")
+    plt.savefig(output_file, dpi=300, bbox_inches="tight")
+
+    # Display the plot
     plt.show()
 
 
-def group_columns_by_property(data, prefix):
+def plot_property_groups(data, prefix):
     """
-    Group prefix columns by unique properties.
+    Group prefix columns by unique properties and create scatter plots for each group.
 
     Args:
         data (pd.DataFrame): Input DataFrame containing Machine columns.
-
-    Returns:
-        dict: A dictionary where keys are unique properties and values are lists of columns.
-    """
-    unique_properties = list(
-        set(
-            map(
-                lambda col: col.split(".", 1)[1],
-                filter(lambda col: col.startswith(prefix), data.columns),
-            )
-        )
-    )
-
-    property_columns = {}
-
-    for prop in unique_properties:
-        prop_columns = [col for col in data.columns if col.endswith(f"{prop}")]
-        property_columns[prop] = prop_columns
-
-    return property_columns
-
-
-def plot_property_groups(data, property_groups):
-    """
-    Create line plots for each group of columns representing a property.
-
-    Args:
-        data (pd.DataFrame): Input DataFrame.
-        property_groups (dict): Dictionary where keys are properties and values are lists of columns.
+        prefix (str): Prefix to filter and group columns.
 
     Returns:
         None
     """
-    for prop, columns in property_groups.items():
+    output_dir = "../../reports/figures/plots"
+    # Create the output directory if it doesn't exist
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Group columns by unique properties
+    unique_properties = list(
+        set(
+            map(
+                lambda col: col.split(".", 1)[1],
+                filter(lambda col: prefix in col, data.columns),
+            )
+        )
+    )
+
+    for prop in unique_properties:
+        prop_columns = [col for col in data.columns if col.endswith(f"{prop}")]
         plt.figure(figsize=(12, 6))
-        plt.title(f"Line Plot for Property: {prop}")
-        for column in columns:
-            plt.plot(data.index, data[column], label=column)
+        plt.title(f"Scatter Plot for Property: {prop}")
+        for column in prop_columns:
+            label = column.split(".")[
+                -1
+            ]  # Use the last part of the column name as the label
+            plt.scatter(data.index, data[column], s=2, label=label)
         plt.xlabel("Datetime")
         plt.ylabel("Value")
         plt.legend(loc="best")
         plt.grid(True)
+
+        # Save the plot to a file (removing existing file if it exists)
+        output_file = os.path.join(output_dir, f"scatter_plot_{prop}.png")
+        if os.path.exists(output_file):
+            os.remove(output_file)
+        plt.savefig(output_file, dpi=300, bbox_inches="tight")
+
+        # Display the plot
         plt.show()
 
 
 # Create line plot of columns name
-plot_columns_by_prefix(data, "AmbientConditions", "Ambient Conditions")
-plot_columns_by_prefix(data, "FirstStage", "First Stage")
-
-# Create a list of unique properties
-unique_properties_machine = group_columns_by_property(data, "Machine")
-unique_properties_output = group_columns_by_property(data, "Stage")
+plot_columns_by_prefix(data, "AmbientConditions")
+plot_columns_by_prefix(data, "FirstStage")
 
 # Create line plot of columns grouped by property
-plot_property_groups(data, unique_properties_machine)
-plot_property_groups(data, unique_properties_output)
+plot_property_groups(data, "Machine")
+plot_property_groups(data, "Output")
